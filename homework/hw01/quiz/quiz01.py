38445dfcd6c9bfb31e02d8b06d4cd4ecff959317
minwuh081@gmail.com <w860314m>
2019-05-30 21:03:46 +0200

    homework 1

---
+++ homework/hw01/quiz/quiz01.py
@@ -0,0 +1,40 @@
+def multiple(a, b):
+    """Return the smallest number n that is a multiple of both a and b.
+
+    >>> multiple(3, 4)
+    12
+    >>> multiple(14, 21)
+    42
+    """
+    "*** YOUR CODE HERE ***"
+    n=1
+    controll=True
+    while controll:
+        if max(a,b)*n%min(a,b)==0:
+            controll=False
+        else:
+            n+=1
+    return max(a,b)*n
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(1313131) # 1 and 3
+    2
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(10000) # 0 and 1
+    2
+    >>> unique_digits(101) # 0 and 1
+    2
+    >>> unique_digits(10) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    list=[]
+    for i in n:
+        if i not in list:
+            list.append(i)
+    return len(list)
